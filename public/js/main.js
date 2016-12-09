f8cf569d7b87fc8e13d216cf19ba5b91d068672d
zqf1904 <zqf1904@gmail.com>
2016-12-09 17:36:48 -0500

    init commit

---
+++ public/js/main.js
@@ -0,0 +1,350 @@
+// CUSTOM JS FILE //
+var map; // global map variable
+var markers = []; // array to hold map markers
+
+function init() {
+  
+  // set some default map details, initial center point, zoom and style
+  var mapOptions = {
+    center: new google.maps.LatLng(40.74649,-74.0094), // NYC
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  // create the map and reference the div#map-canvas container
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  
+  // get the animals (ajax) 
+  // and render them on the map
+  renderPlaces();
+}
+
+jQuery("#addForm").submit(function(e){
+
+	// first, let's pull out all the values
+	// the name form field value
+	var name = jQuery("#name").val();
+	var age = jQuery("#age").val();
+	var weight = jQuery("#weight").val();
+	var tags = jQuery("#tags").val();
+	var breed = jQuery("#breed").val();
+	var url = jQuery("#url").val();
+	var location = jQuery("#location").val();
+
+		// make sure we have a location
+	if(!location || location=="") return alert('We need a location!');
+
+	// POST the data from above to our API create route
+  jQuery.ajax({
+  	url : '/api/create',
+  	dataType : 'json',
+  	type : 'POST',
+  	// we send the data in a data object (with key/value pairs)
+  	data : {
+  		name : name,
+  		age : age,
+  		tags : tags,
+  		breed : breed,
+  		weight: weight,
+  		url : url,
+  		location : location
+  	},
+  	success : function(response){
+  		if(response.status=="OK"){
+	  		// success
+	  		console.log(response);
+	  		// re-render the map
+	  		renderPlaces();
+	  		// now, clear the input fields
+	  		jQuery("#addForm input").val('');
+  		}
+  		else {
+  			alert("something went wrong");
+  		}
+  	},
+  	error : function(err){
+  		// do error checking
+  		alert("something went wrong");
+  		console.error(err);
+  	}
+  });
+
+	// prevents the form from submitting normally
+  e.preventDefault();
+  return false;
+
+});
+
+
+
+
+
+
+
+
+
+// // when the addForm is submitted (with a new animal), the below runs
+// jQuery("#addForm").submit(function(e){
+
+// 	// first, let's pull out all the values
+// 	// the name form field value
+// 	var name = jQuery("#name").val();
+// 	var age = jQuery("#age").val();
+// 	var weight = jQuery("#weight").val();
+// 	var tags = jQuery("#tags").val();
+// 	var breed = jQuery("#breed").val();
+// 	var url = jQuery("#url").val();
+// 	var location = jQuery("#location").val();
+
+// 	// make sure we have a location
+// 	if(!location || location=="") return alert('We need a location!');
+      
+// 	// POST the data from above to our API create route
+//   jQuery.ajax({
+//   	url : '/api/create',
+//   	dataType : 'json',
+//   	type : 'POST',
+//   	// we send the data in a data object (with key/value pairs)
+//   	data : {
+//   		name : name,
+//   		age : age,
+//   		tags : tags,
+//   		breed : breed,
+//   		weight: weight,
+//   		url : url,
+//   		location : location
+//   	},
+//   	success : function(response){
+//   		if(response.status=="OK"){
+// 	  		// success
+// 	  		console.log(response);
+// 	  		// re-render the map
+// 	  		renderPlaces();
+// 	  		// now, clear the input fields
+// 	  		jQuery("#addForm input").val('');
+//   		}
+//   		else {
+//   			alert("something went wrong");
+//   		}
+//   	},
+//   	error : function(err){
+//   		// do error checking
+//   		alert("something went wrong");
+//   		console.error(err);
+//   	}
+//   }); 
+
+// 	// prevents the form from submitting normally
+//   e.preventDefault();
+//   return false;
+// });
+
+// get Animals JSON from /api/get
+// loop through and populate the map with markers
+var renderPlaces = function() {
+	var infowindow =  new google.maps.InfoWindow({
+	    content: ''
+	});
+
+	jQuery.ajax({
+		url : '/api/get',
+		dataType : 'json',
+		success : function(response) {
+
+			console.log(response);
+			animals = response.animals;
+			// first clear any existing markers, because we will re-add below
+			clearMarkers();
+			markers = [];
+
+			// now, loop through the animals and add them as markers to the map
+			for(var i=0;i<animals.length;i++){
+
+				var latLng = {
+					lat: animals[i].location.geo[1], 
+					lng: animals[i].location.geo[0]
+				}
+
+				// make and place map maker.
+				var marker = new google.maps.Marker({
+				    map: map,
+				    position: latLng,
+				    title : animals[i].name + "<br>" + animals[i].breed + "<br>" + animals[i].location.name
+				});
+
+				bindInfoWindow(marker, map, infowindow, '<b>'+animals[i].name + "</b> ("+animals[i].breed+") <br>" + animals[i].location.name);
+				
+				// keep track of markers
+				markers.push(marker);
+			}
+
+			// now, render the animal image/data
+			renderAnimals(animals);
+
+		}
+	})
+};
+
+// edit form button event
+// when the form is submitted (with a new animal edit), the below runs
+jQuery("#editForm").submit(function(e){
+
+	// first, let's pull out all the values
+	// the name form field value
+	var name = jQuery("#edit-name").val();
+	var age = jQuery("#edit-age").val();
+	var weight = jQuery("#editWeight").val();
+	var tags = jQuery("#edit-tags").val();
+	var breed = jQuery("#edit-breed").val();
+	var url = jQuery("#edit-url").val();
+	var location = jQuery("#edit-location").val();
+	var id = jQuery("#edit-id").val();
+
+	// make sure we have a location
+	if(!location || location=="") return alert('We need a location!');
+     
+  console.log(id);
+      
+	// POST the data from above to our API create route
+  jQuery.ajax({
+  	url : '/api/update/'+id,
+  	dataType : 'json',
+  	type : 'POST',
+  	// we send the data in a data object (with key/value pairs)
+  	data : {
+  		name : name,
+  		age : age,
+  		tags : tags,
+  		breed : breed,
+  		weight: weight,
+  		url : url,
+  		location : location
+  	},
+  	success : function(response){
+  		if(response.status=="OK"){
+	  		// success
+	  		console.log(response);
+	  		// re-render the map
+	  		renderPlaces();
+	  		// now, close the modal
+	  		$('#editModal').modal('hide')
+	  		// now, clear the input fields
+	  		jQuery("#editForm input").val('');
+  		}
+  		else {
+  			alert("something went wrong");
+  		}
+  	},
+  	error : function(err){
+  		// do error checking
+  		alert("something went wrong");
+  		console.error(err);
+  	}
+  }); 
+
+	// prevents the form from submitting normally
+  e.preventDefault();
+  return false;
+});
+
+// binds a map marker and infoWindow together on click
+var bindInfoWindow = function(marker, map, infowindow, html) {
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(html);
+        infowindow.open(map, marker);
+    });
+}
+
+function renderAnimals(animals){
+
+	// first, make sure the #animal-holder is empty
+	jQuery('#animal-holder').empty();
+
+	// loop through all the animals and add them in the animal-holder div
+	for(var i=0;i<animals.length;i++){
+		var htmlToAdd = '<div class="col-md-4 animal">'+
+			'<img class="url" src="'+animals[i].url+'">'+
+			'<h1 class="name">'+animals[i].name+'</h1>'+
+			'<ul>'+
+				'<li>Location: <span class="location">'+animals[i].location.name+'</span></li>'+
+				'<li>Breed: <span class="breed">'+animals[i].breed+'</span></li>'+
+				'<li>Age: <span class="age">'+animals[i].age+'</span></li>'+
+				'<li>Weight: <span class="weight">'+animals[i].weight+'</span></li>'+
+				'<li>Tags: <span class="tags">'+animals[i].tags+'</span></li>'+
+				'<li class="hide id">'+animals[i]._id+'</li>'+
+			'</ul>'+
+			'<button type="button" id="'+animals[i]._id+'" onclick="deleteAnimal(event)">Delete Animal</button>'+
+			'<button type="button" data-toggle="modal" data-target="#editModal"">Edit Animal</button>'+
+		'</div>';
+
+		jQuery('#animal-holder').prepend(htmlToAdd);
+
+	}
+}
+
+jQuery('#editModal').on('show.bs.modal', function (e) {
+  // let's get access to what we just clicked on
+  var clickedButton = e.relatedTarget;
+  // now let's get its parent
+	var parent = jQuery(clickedButton).parent();
+
+  // now, let's get the values of the pet that we're wanting to edit
+  // we do this by targeting specific spans within the parent and pulling out the text
+  var name = $(parent).find('.name').text();
+  var age = $(parent).find('.age').text();
+  var weight = $(parent).find('.weight').text();
+  var tags = $(parent).find('.tags').text();
+  var breed = $(parent).find('.breed').text();
+  var url = $(parent).find('.url').attr('src');
+  var location = $(parent).find('.location').text();
+  var id = $(parent).find('.id').text();
+
+  // now let's set the value of the edit fields to those values
+ 	jQuery("#edit-name").val(name);
+	jQuery("#edit-age").val(age);
+	jQuery("#editWeight").val(weight);
+	jQuery("#edit-tags").val(tags);
+	jQuery("#edit-breed").val(breed);
+	jQuery("#edit-url").val(url);
+	jQuery("#edit-location").val(location);
+	jQuery("#edit-id").val(id);
+
+})
+
+
+function deleteAnimal(event){
+	var targetedId = event.target.id;
+	console.log('the animal to delete is ' + targetedId);
+
+	// now, let's call the delete route with AJAX
+	jQuery.ajax({
+		url : '/api/delete/'+targetedId,
+		dataType : 'json',
+		success : function(response) {
+			// now, let's re-render the animals
+
+			renderPlaces();
+
+		}
+	})
+
+	event.preventDefault();
+}
+
+function clearMarkers(){
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null); // clears the markers
+  }	
+}
+
+// when page is ready, initialize the map!
+google.maps.event.addDomListener(window, 'load', init);
+
+
+//window.addEventListener('load',init);
+
+
+
+
+
+
---
+++ routes/index.js
@@ -0,0 +1,313 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose'); // mongoDB library
+var geocoder = require('geocoder'); // geocoder library
+
+// our db model
+var Animal = require("../models/model.js");
+
+/**
+ * GET '/'
+ * Default home route. Just relays a success message back.
+ * @param  {Object} req
+ * @return {Object} json
+ */
+router.get('/', function(req, res) {
+  
+  var jsonData = {
+  	'name': 'pets-of-nyc',
+  	'api-status':'OK'
+  }
+
+  // respond with json data
+  res.json(jsonData)
+});
+
+// simple route to show the pets html
+router.get('/pets', function(req,res){
+  res.render('pets.html');
+})
+
+// /**
+//  * POST '/api/create'
+//  * Receives a POST request of the new user and location, saves to db, responds back
+//  * @param  {Object} req. An object containing the different attributes of the Person
+//  * @return {Object} JSON
+//  */
+
+router.post('/api/create', function(req, res){
+
+    console.log('the data we received is --> ')
+    console.log(req.body);
+
+    
+    // pull out the information from the req.body
+    var name = req.body.name;
+    var age = req.body.age;
+    var tags = req.body.tags.split(","); // split string into array
+    var weight = req.body.weight;
+    var breed = req.body.breed;
+    var url = req.body.url;
+    var location = req.body.location;
+
+    // hold all this data in an object
+    // this object should be structured the same way as your db model
+    var animalObj = {
+      name: name,
+      age: age,
+      tags: tags,
+      weight: weight,
+      breed: breed,
+      url: url
+    };
+
+    // if there is no location, return an error
+    if(!location) return res.json({status:'ERROR', message: 'You are missing a required field or have submitted a malformed request.'})
+
+    // now, let's geocode the location
+    geocoder.geocode(location, function (err,data) {
+
+
+      // if we get an error, or don't have any results, respond back with error
+      if (!data || data==null || err || data.status == 'ZERO_RESULTS'){
+        var error = {status:'ERROR', message: 'Error finding location'};
+        return res.json({status:'ERROR', message: 'You are missing a required field or have submitted a malformed request.'})
+      }
+
+      // else, let's pull put the lat lon from the results
+      var lon = data.results[0].geometry.location.lng;
+      var lat = data.results[0].geometry.location.lat;
+
+      // now, let's add this to our animal object from above
+      animalObj.location = {
+        geo: [lon,lat], // need to put the geo co-ordinates in a lng-lat array for saving
+        name: data.results[0].formatted_address // the location name
+      }
+
+      // now, let's save it to the database
+      // create a new animal model instance, passing in the object we've created
+      var animal = new Animal(animalObj);
+
+      // now, save that animal instance to the database
+      // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save    
+      animal.save(function(err,data){
+        // if err saving, respond back with error
+        if (err){
+          var error = {status:'ERROR', message: 'Error saving animal'};
+          return res.json(error);
+        }
+
+        console.log('saved a new animal!');
+        console.log(data);
+
+        // now return the json data of the new animal
+        var jsonData = {
+          status: 'OK',
+          animal: data
+        }
+
+        return res.json(jsonData);
+
+      }) 
+
+    }); 
+});
+
+// /**
+//  * GET '/api/get/:id'
+//  * Receives a GET request specifying the animal to get
+//  * @param  {String} req.param('id'). The animalId
+//  * @return {Object} JSON
+//  */
+
+router.get('/api/get/:id', function(req, res){
+
+  var requestedId = req.param('id');
+
+  // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findById
+  Animal.findById(requestedId, function(err,data){
+
+    // if err or no user found, respond with error 
+    if(err || data == null){
+      var error = {status:'ERROR', message: 'Could not find that animal'};
+       return res.json(error);
+    }
+
+    // otherwise respond with JSON data of the animal
+    var jsonData = {
+      status: 'OK',
+      animal: data
+    }
+
+    return res.json(jsonData);
+  
+  })
+})
+
+// /**
+//  * GET '/api/get'
+//  * Receives a GET request to get all animal details
+//  * @return {Object} JSON
+//  */
+
+router.get('/api/get', function(req, res){
+
+  // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
+  Animal.find(function(err, data){
+    // if err or no animals found, respond with error 
+    if(err || data == null){
+      var error = {status:'ERROR', message: 'Could not find animals'};
+      return res.json(error);
+    }
+
+    // otherwise, respond with the data 
+
+    var jsonData = {
+      status: 'OK',
+      animals: data
+    } 
+
+    res.json(jsonData);
+
+  })
+
+})
+
+// /**
+//  * POST '/api/update/:id'
+//  * Receives a POST request with data of the animal to update, updates db, responds back
+//  * @param  {String} req.param('id'). The animalId to update
+//  * @param  {Object} req. An object containing the different attributes of the Animal
+//  * @return {Object} JSON
+//  */
+
+router.post('/api/update/:id', function(req, res){
+
+   var requestedId = req.param('id');
+
+   var dataToUpdate = {}; // a blank object of data to update
+
+    // pull out the information from the req.body and add it to the object to update
+    var name, age, weight, breed, url, location; 
+
+    // we only want to update any field if it actually is contained within the req.body
+    // otherwise, leave it alone.
+    if(req.body.name) {
+      name = req.body.name;
+      // add to object that holds updated data
+      dataToUpdate['name'] = name;
+    }
+    if(req.body.age) {
+      age = req.body.age;
+      // add to object that holds updated data
+      dataToUpdate['age'] = age;
+    }
+    if(req.body.weight) {
+      weight = req.body.weight;
+      // add to object that holds updated data
+      dataToUpdate['weight'] = weight;
+    }
+    if(req.body.breed) {
+      breed = req.body.breed;
+      // add to object that holds updated data
+      dataToUpdate['breed'] = breed;
+    }
+    if(req.body.url) {
+      url = req.body.url;
+      // add to object that holds updated data
+      dataToUpdate['url'] = url;
+    }
+
+    var tags = []; // blank array to hold tags
+    if(req.body.tags){
+      tags = req.body.tags.split(","); // split string into array
+      // add to object that holds updated data
+      dataToUpdate['tags'] = tags;
+    }
+
+    if(req.body.location) {
+      location = req.body.location;
+    }
+
+    // if there is no location, return an error
+    if(!location) return res.json({status:'ERROR', message: 'You are missing a required field or have submitted a malformed request.'})
+
+    // now, let's geocode the location
+    geocoder.geocode(location, function (err,data) {
+
+
+      // if we get an error, or don't have any results, respond back with error
+      if (!data || data==null || err || data.status == 'ZERO_RESULTS'){
+        var error = {status:'ERROR', message: 'Error finding location'};
+        return res.json({status:'ERROR', message: 'You are missing a required field or have submitted a malformed request.'})
+      }
+
+      // else, let's pull put the lat lon from the results
+      var lon = data.results[0].geometry.location.lng;
+      var lat = data.results[0].geometry.location.lat;
+
+      // now, let's add this to our animal object from above
+      dataToUpdate['location'] = {
+        geo: [lon,lat], // need to put the geo co-ordinates in a lng-lat array for saving
+        name: data.results[0].formatted_address // the location name
+      }
+
+      console.log('the data to update is ' + JSON.stringify(dataToUpdate));
+
+      // now, update that animal
+      // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
+      Animal.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
+        // if err saving, respond back with error
+        if (err){
+          var error = {status:'ERROR', message: 'Error updating animal'};
+          return res.json(error);
+        }
+
+        console.log('updated the animal!');
+        console.log(data);
+
+        // now return the json data of the new person
+        var jsonData = {
+          status: 'OK',
+          animal: data
+        }
+
+        return res.json(jsonData);
+
+      })
+
+    });     
+
+})
+
+/**
+ * GET '/api/delete/:id'
+ * Receives a GET request specifying the animal to delete
+ * @param  {String} req.param('id'). The animalId
+ * @return {Object} JSON
+ */
+
+router.get('/api/delete/:id', function(req, res){
+
+  var requestedId = req.param('id');
+
+  // Mongoose method to remove, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
+  Animal.findByIdAndRemove(requestedId,function(err, data){
+    if(err || data == null){
+      var error = {status:'ERROR', message: 'Could not find that animal to delete'};
+      return res.json(error);
+    }
+
+    // otherwise, respond back with success
+    var jsonData = {
+      status: 'OK',
+      message: 'Successfully deleted id ' + requestedId
+    }
+
+    res.json(jsonData);
+
+  })
+
+})
+
+module.exports = router;
